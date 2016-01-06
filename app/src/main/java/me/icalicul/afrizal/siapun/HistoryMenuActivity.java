adca751feb47a11030e627c3a635bc857fb0a9bc
icalF <laser.survivor@gmail.com>
2016-01-06 21:37:40 +0700

    title page skeleton

---
+++ app/src/main/java/me/icalicul/afrizal/siapun/HistoryMenuActivity.java
@@ -0,0 +1,13 @@
+package me.icalicul.afrizal.siapun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_menu);
+    }
+}
---
+++ app/src/main/java/me/icalicul/afrizal/siapun/TitleActivity.java
@@ -0,0 +1,23 @@
+package me.icalicul.afrizal.siapun;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+
+    public void gotoExerciseMenu(View view) {
+        startActivity(new Intent(getApplicationContext(), ExerciseMenuActivity.class));
+    }
+
+    public void gotoHistoryMenu(View view) {
+        startActivity(new Intent(getApplicationContext(), HistoryMenuActivity.class));
+    }
+}
